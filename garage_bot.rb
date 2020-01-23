b336640008cd4755cbad9defc5e17a7bac773a06
Tomas Landovsky <landovsky@gmail.com>
2020-01-23 23:08:33 +0100

    WIP: slack msg builder

--- garage_bot.rb
+++ garage_bot.rb
@@ -30,99 +30,61 @@ class GarageBot
     }
   end
 
+  def self.header
+    {
+      "type": "section",
+      "block_id": "EJKJEI",
+      "text": {
+        "type": "plain_text",
+        "emoji": true,
+        "text": "Your parking for 14.5. - 22.4."
+      }
+    }
+  end
+
+  def self.divider
+    {
+      "type": "divider"
+    }
+  end
+
+  def self.day(name, status, button = nil)
+    base = {
+      "type": "section",
+            "block_id": "EJKJ2EI-#{name}",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*#{name.upcase}*\n#{status}"
+      }
+    }
+    button ? base.merge(accessory: button) : base
+  end
+
+  def self.button(type, day)
+    base = {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "emoji": true,
+        "text": type.to_s.upcase
+      },
+      "value": "#{type}-#{day}"
+    }
+    type == :cancel ? base.merge('style': 'danger') : base
+  end
+
   def self.blocks
     {
       "replace_original": true,
       "blocks": [
+        header,
+        divider,
+        day('monday', 'park on spot 553', button(:book, 'monday')),
+        day('tuesday', ':car: parking available', button(:cancel, 'tuesday')),
+        day('wednesday', 'all places are taken'),
+        day('thursday', 'park on spot 553', button(:cancel, 'thursday')),
+        day('friday', ':car: parking available', button(:book, 'friday')),
         {
-          "type": "section",
-          "block_id": "EJKJEI",
-          "text": {
-            "type": "plain_text",
-            "emoji": true,
-            "text": "Your parking for 14.5. - 22.4."
-          }
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "section",
-                "block_id": "EJKJ2EI-monday",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Monday*\npark on spot 553"
-          },
-          "accessory": {
-            "type": "button",
-                    "style": "danger",
-            "text": {
-              "type": "plain_text",
-              "emoji": true,
-              "text": "Cancel"
-            },
-            "value": "cancel-monday"
-          }
-        },
-            {
-          "type": "section",
-                "block_id": "EJKJEI-tuesday",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Tuesday*\n:car: parking available"
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "emoji": true,
-              "text": "Reserve"
-            },
-            "value": "book-tuesday"
-          }
-        },
-            {
-          "type": "section",
-                "block_id": "EJKJEI-wednesday",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Wednesday*\nall places are taken"
-          }
-        },
-            {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Thursday*\npark on spot 534"
-          },
-          "accessory": {
-            "type": "button",
-                    "style": "danger",
-            "text": {
-              "type": "plain_text",
-              "emoji": true,
-              "text": "Cancel"
-            },
-            "value": "cancel-thursday"
-          }
-        },
-            {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Friday*\n:car: parking available"
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "emoji": true,
-              "text": "Reserve"
-            },
-            "value": "book-friday"
-          }
-        },
-            {
           "type": "section",
           "text": {
             "type": "mrkdwn",
