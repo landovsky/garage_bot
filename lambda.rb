0ba80a80c8c3f700d58ab41f062b07d21c90b80c
Tomas Landovsky <landovsky@gmail.com>
2020-02-01 13:28:41 +0100

    Updated lambda.handler

--- lambda.rb
+++ lambda.rb
@@ -4,7 +4,6 @@ require 'json'
 require_relative 'app/controller'
 
 def handler(event:, context:)
-  request_data = URI.decode_www_form(event['body']).to_h
-  response_body = Controller.call(request_data)
+  response_body = Controller.call(event['body'])
   { statusCode: 200, body: response_body.to_json }
 end
