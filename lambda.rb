a76ed7ee1f7f24d72aeaa7e9713c5cb9d0849aeb
Tomas Landovsky <landovsky@gmail.com>
2020-02-01 13:35:10 +0100

    Updated lambda.handler

--- lambda.rb
+++ lambda.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require 'json'
-require_relative 'app/controller'
+require_relative 'app/slack_router'
 
 def handler(event:, context:)
-  response_body = Controller.call(event['body'])
+  response_body = SlackRouter.call(event['body'])
   { statusCode: 200, body: response_body.to_json }
 end
