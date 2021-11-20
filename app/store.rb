a424e8d9c0b096caec2324af3775f17ed3e0aac6
Tomas Landovsky <landovsky@gmail.com>
2021-11-20 17:28:56 +0100

    Re-numbered parking spots

--- app/store.rb
+++ app/store.rb
@@ -6,7 +6,7 @@ require_relative 'dynamo'
 module Store
   def self.all_spots(building)
     data = {
-      Garage::RIVER => [155, 156, 157, 158, 159, 160, 161, 165, 191, 197, 199, 200, 201, 202]
+      Garage::RIVER => [26, 27, 155, 156, 157, 158, 159, 160, 161, 165, 191, 197, 199, 200]
     }
     data[building]
   end
